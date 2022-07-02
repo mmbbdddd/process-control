093cb1353e1e81f2133f79cb5ee26f44fb17465e
wanglin <81525144@qq.com>
2022-07-02 21:08:17 +0800

    增加拦截器机制。相关代码未完成  :还缺少xml解析机制和工厂构建机制。

--- src/main/java/io/ddbm/pc/Coast.java
+++ src/main/java/io/ddbm/pc/Coast.java
@@ -2,11 +2,11 @@ package io.ddbm.pc;
 
 public class Coast {
     public static final Integer DEFAULT_RETRY = 10;
-    public static final String DEFAULT_EVENT = "next";
-    public static final String TOTAL_ERROR  = "total_error";
-    public static final String     TOTAL_COUNT = "total_count";
+    public static final String  DEFAULT_EVENT = "next";
+    public static final String  TOTAL_ERROR   = "total_error";
+    public static final String  TOTAL_COUNT   = "total_count";
 
     public static String EVENT_COUNT(Event event) {
-        return null;
+        return String.format("%s_%s_%s", event.getOn().name, event.getEvent(), "count");
     }
 }
--- src/main/java/io/ddbm/pc/support/ActionPlugins.java
+++ src/main/java/io/ddbm/pc/support/ActionPlugins.java
@@ -1,6 +1,7 @@
 package io.ddbm.pc.support;
 
 import io.ddbm.pc.Action;
+import io.ddbm.pc.Coast;
 import io.ddbm.pc.FlowContext;
 import io.ddbm.pc.exception.InterruptException;
 import io.ddbm.pc.exception.PauseException;
@@ -28,15 +29,23 @@ public class ActionPlugins {
                 for (Action action : actions) {
                     action.execute(ctx);
                 }
+                Integer c = (Integer) ctx.getSession().get(Coast.EVENT_COUNT(ctx.getEvent()), 0);
+                ctx.getSession().set(Coast.EVENT_COUNT(ctx.getEvent()), c + 1);
             }
             postAction(ctx, this);
         } catch (PauseException | InterruptException e) {
+            Integer c = (Integer) ctx.getSession().get(Coast.TOTAL_ERROR, 0);
+            ctx.getSession().set(Coast.TOTAL_ERROR, c + 1);
             onActionError(ctx, this, e);
             throw e;
         } catch (Exception e) {
+            Integer c = (Integer) ctx.getSession().get(Coast.TOTAL_ERROR, 0);
+            ctx.getSession().set(Coast.TOTAL_ERROR, c + 1);
             onActionError(ctx, this, e);
             throw new PauseException(ctx, e);
         } finally {
+            Integer c = (Integer) ctx.getSession().get(Coast.TOTAL_COUNT, 0);
+            ctx.getSession().set(Coast.TOTAL_COUNT, c + 1);
             actionFinally(ctx, this);
         }
     }
