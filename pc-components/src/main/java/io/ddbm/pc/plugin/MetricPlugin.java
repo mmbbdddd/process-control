7d36cf30c2edd2acf6fd8ba4102051cf44553de2
wanglin <81525144@qq.com>
2022-07-10 18:55:15 +0800

    组件、核心 模块分离

---
+++ pc-components/src/main/java/io/ddbm/pc/plugin/MetricPlugin.java
@@ -0,0 +1,26 @@
+package io.ddbm.pc.plugin;
+
+import io.ddbm.pc.FlowContext;
+import io.ddbm.pc.Plugin;
+
+public class MetricPlugin implements Plugin {
+    @Override
+    public void preAction(FlowContext ctx) {
+
+    }
+
+    @Override
+    public void postAction(FlowContext ctx) {
+
+    }
+
+    @Override
+    public void actionFinally(FlowContext ctx) {
+
+    }
+
+    @Override
+    public void onActionError(FlowContext ctx, Exception e) {
+
+    }
+}
