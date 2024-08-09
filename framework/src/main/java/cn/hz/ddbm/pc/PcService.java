ae177b82deb7ca4f042ca81a90b388bdfb807e30
wanglilin <wanglilin@itbox.cn>
2024-08-09 09:58:27 +0800

    修改工程架构

---
+++ framework/src/main/java/cn/hz/ddbm/pc/PcService.java
@@ -0,0 +1,40 @@
+package cn.hz.ddbm.pc;
+
+import cn.hutool.core.lang.Assert;
+import cn.hutool.core.util.StrUtil;
+import cn.hz.ddbm.pc.core.Flow;
+import cn.hz.ddbm.pc.core.FlowContext;
+import cn.hz.ddbm.pc.core.FlowPayload;
+import cn.hz.ddbm.pc.core.coast.Coasts;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class PcService {
+    Map<String, Flow> flows = new HashMap<>();
+
+
+    public <T extends FlowPayload> void batchExecute(String flowName, List<T> payloads, String event) {
+        Assert.notNull(flowName, "flowName is null");
+        Assert.notNull(payloads, "FlowPayload is null");
+        event = StrUtil.isBlank(event) ? Coasts.EVENT_DEFAULT : event;
+        Flow flow = flows.get(flowName);
+
+        for (T payload : payloads) {
+            FlowContext<T> ctx = new FlowContext<>(flow, payload, event);
+            ctx.getFlow().execute(ctx);
+        }
+    }
+
+    public <T extends FlowPayload> void execute(String flowName, T payload, String event) {
+        Assert.notNull(flowName, "flowName is null");
+        Assert.notNull(payload, "FlowPayload is null");
+        event = StrUtil.isBlank(event) ? Coasts.EVENT_DEFAULT : event;
+        Flow           flow = flows.get(flowName);
+        FlowContext<T> ctx  = new FlowContext<>(flow, payload, event);
+        ctx.getFlow().execute(ctx);
+    }
+
+
+}
