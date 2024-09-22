3282a4dba4b5aad816db9bb7227ac4d61b8ebcba
mmbbdddd <81521544@qq.com>
2024-09-22 11:45:04 +0800

    saga单元测试

--- core/src/main/java/cn/hz/ddbm/pc/newcore/fsm/workers/FsmLocalWorker.java
+++ core/src/main/java/cn/hz/ddbm/pc/newcore/fsm/workers/FsmLocalWorker.java
@@ -23,27 +23,20 @@ public class FsmLocalWorker extends FsmWorker {
     public void execute(FlowContext<FsmState> ctx) throws Exception {
         ctx.setAction(action);
         //如果任务可执行
-        Offset offset = ctx.state.offset;
-        switch (offset) {
-            case task:
-                Object result = action.doLocalFsm(ctx);
-                ctx.metricsState();
-                Enum state = router.router(ctx, result);
-                if (null == state) {
-                    //如果结果为空，路由无结果，1，暂停，2报错
-                    ctx.state.flowStatus = FlowStatus.MANUAL;
-                    ctx.errorMessage     = ErrorCode.ROUTER_RESULT_EMPTY;
-                } else if (state.equals(ctx.getState().state)) {
-                    //如果状态不变，在告警（todo）告警，告警，告警，或者人工处理把。
-                    ctx.state.flowStatus = FlowStatus.MANUAL;
-                    ctx.errorMessage     = "节点执行后状态不变！！";
-                } else {
-                    ctx.state.state  = state;
-                    ctx.state.offset = Offset.task;
-                }
-                break;
-            case failover:
-                break;
+        Object result = action.doLocalFsm(ctx);
+        ctx.metricsState();
+        Enum state = router.router(ctx, result);
+        if (null == state) {
+            //如果结果为空，路由无结果，1，暂停，2报错
+            ctx.state.flowStatus = FlowStatus.MANUAL;
+            ctx.errorMessage     = ErrorCode.ROUTER_RESULT_EMPTY;
+        } else if (state.equals(ctx.getState().state)) {
+            //如果状态不变，在告警（todo）告警，告警，告警，或者人工处理把。
+            ctx.state.flowStatus = FlowStatus.MANUAL;
+            ctx.errorMessage     = "节点执行后状态不变！！";
+        } else {
+            ctx.state.state  = state;
+            ctx.state.offset = Offset.task;
         }
     }
 
--- core/src/main/java/cn/hz/ddbm/pc/newcore/fsm/workers/FsmRemoteWorker.java
+++ core/src/main/java/cn/hz/ddbm/pc/newcore/fsm/workers/FsmRemoteWorker.java
@@ -36,10 +36,13 @@ public class FsmRemoteWorker extends FsmWorker {
             case failover:
                 Object result = action.remoteFsmQuery(ctx);
                 ctx.metricsState();
-                Enum state = router.router(ctx, result);
+                Enum state = router.router(ctx,result);
                 if (null == state) {
                     ctx.state.flowStatus = FlowStatus.MANUAL;
                     ctx.errorMessage     = ErrorCode.ROUTER_RESULT_EMPTY;
+                } else if(state.equals(ctx.getState().state)) {
+                    ctx.state.flowStatus = FlowStatus.MANUAL;
+                    ctx.errorMessage = "";
                 } else {
                     ctx.state.state  = state;
                     ctx.state.offset = Offset.task;
